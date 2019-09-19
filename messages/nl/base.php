e20b4dfe21fe7105a7d514a7d25abaa1b045b36e
HumHub Translations <info@humhub.com>
2019-09-19 19:49:23 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/base.php
+++ messages/nl/base.php
@@ -5,6 +5,6 @@ return array (
   'From which registration date should new users displayed as new?' => 'Vanaf welke registratiedatum moeten nieuwe gebruikers als nieuw worden weergegeven?',
   'Save' => 'Opslaan',
   'The number of most actice users that will be shown.' => 'Het aantal meest actieve gebruikers dat wordt getoond.',
-  'The panel title for the dashboard widget.' => '',
+  'The panel title for the dashboard widget.' => 'Titel in het dashboard',
   'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Deze waarde is misschien nodig na een import van bestaande gebruikers. Laat het leeg als uw gebruikersbestand vanzelf groeit.',
 );
