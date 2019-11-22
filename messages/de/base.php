540b863b36b3a3a0ef820090a0c4cac9ae96e7e8
HumHub Translations <info@humhub.com>
2019-11-22 17:21:06 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => '<strong>Neue Benutzer</strong> Modul Konfiguration',
-    'Back to modules' => 'Zurück zu den Modulen',
-    'From which registration date should new users displayed as new?' => 'Ab welchem Registrier-Datum sollen Benutzer als „neu“ angezeigt werden?',
-    'Save' => 'Speichern',
-    'The panel title for the dashboard widget.' => 'Der Titel für das Dashboard-Widget.',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Dieser Wert ist möglicherweise notwendig, falls bestehende Nutzer importiert wurden. Dieses Feld leer lassen wenn die Nutzeranzahl natürlich wächst.',
-    'The number of most active users that will be shown.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong>Neue Benutzer</strong> Modul Konfiguration',
+  'Back to modules' => 'Zurück zu den Modulen',
+  'From which registration date should new users displayed as new?' => 'Ab welchem Registrier-Datum sollen Benutzer als „neu“ angezeigt werden?',
+  'Save' => 'Speichern',
+  'The number of most active users that will be shown.' => 'Wie viele aktivste Nutzer sollen angezeigt werden?',
+  'The panel title for the dashboard widget.' => 'Der Titel für das Dashboard-Widget.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Dieser Wert ist möglicherweise notwendig, falls bestehende Nutzer importiert wurden. Dieses Feld leer lassen wenn die Nutzeranzahl natürlich wächst.',
+);
