259e457edf950784f02e534033b4b8a8e4828911
HumHub Translations <info@humhub.com>
2019-12-03 10:55:06 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/base.php
+++ messages/nl/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => '<strong>Nieuwe leden strong&gt; moduleconfiguratie</strong>',
-    'Back to modules' => 'Terug naar modules',
-    'From which registration date should new users displayed as new?' => 'Vanaf welke registratiedatum moeten nieuwe gebruikers als nieuw worden weergegeven?',
-    'Save' => 'Opslaan',
-    'The panel title for the dashboard widget.' => 'Titel in het dashboard',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Deze waarde is misschien nodig na een import van bestaande gebruikers. Laat het leeg als uw gebruikersbestand vanzelf groeit.',
-    'The number of most active users that will be shown.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong>Nieuwe leden strong&gt; moduleconfiguratie</strong>',
+  'Back to modules' => 'Terug naar modules',
+  'From which registration date should new users displayed as new?' => 'Vanaf welke registratiedatum moeten nieuwe gebruikers als nieuw worden weergegeven?',
+  'Save' => 'Opslaan',
+  'The number of most active users that will be shown.' => 'Het aantal meest actieve gebruikers dat wordt getoond.',
+  'The panel title for the dashboard widget.' => 'Titel in het dashboard',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Deze waarde is misschien nodig na een import van bestaande gebruikers. Laat het leeg als uw gebruikersbestand vanzelf groeit.',
+);
