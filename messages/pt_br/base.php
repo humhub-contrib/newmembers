2f53dbc7bfd6d6fc7364d48b9592b9e36d8db495
HumHub Translations <info@humhub.com>
2020-06-06 12:42:34 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt_br/base.php
+++ messages/pt_br/base.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  '<strong>New Members</strong> Module Configuration' => '',
+  '<strong>New Members</strong> Module Configuration' => '<strong> Novos membros </strong> Configuração do módulo',
   'Back to modules' => 'Voltar aos módulos',
-  'From which registration date should new users displayed as new?' => '',
+  'From which registration date should new users displayed as new?' => 'A partir de qual data de registro os novos usuários devem ser exibidos como novos?',
   'Save' => 'Salvar',
-  'The number of most actice users that will be shown.' => '',
-  'The panel title for the dashboard widget.' => '',
-  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
+  'The number of most actice users that will be shown.' => 'O número da maioria dos usuários ativos que serão mostrados.',
+  'The panel title for the dashboard widget.' => 'O título do painel para o widget do painel.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Talvez esse valor seja necessário após a importação de usuários existentes. Deixe vazio se sua base de usuários cresce naturalmente.',
 );
