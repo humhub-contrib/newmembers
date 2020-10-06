570780c53b9ed5c1c53fce22831ede68ff6a889e
Lucas Bartholemy <lucas@bartholemy.com>
2020-10-06 17:11:24 +0200

    Updated module.sjon

--- Module.php
+++ Module.php
@@ -9,16 +9,27 @@ use humhub\modules\newmembers\widgets\NewMembersSidebarWidget;
 class Module extends \humhub\components\Module
 {
 
+    /**
+     * @inerhitdoc
+     */
+    public $resourcesPath = 'resources';
+
     public static function onSidebarInit($event)
     {
         $event->sender->addWidget(NewMembersSidebarWidget::class, [], ['sortOrder' => 300]);
     }
 
+    /**
+     * @inerhitdoc
+     */
     public function getConfigUrl()
     {
         return Url::to(['/newmembers/config/config']);
     }
 
+    /**
+     * @inerhitdoc
+     */
     public function enable()
     {
         parent::enable();
