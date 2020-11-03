de4e1138849fadbe79ee2906fbb08fec9eb65812
HumHub Translations <info@humhub.com>
2020-11-03 11:03:32 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/bg/base.php
+++ messages/bg/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    'Back to modules' => 'Върни се при модулите',
-    'Save' => 'Запази',
-    '<strong>New Members</strong> Module Configuration' => '',
-    'From which registration date should new users displayed as new?' => '',
-    'The number of most active users that will be shown.' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => 'Конфигурация на модули за <strong>Нови Членове</strong>',
+  'Back to modules' => 'Връщане при модулите',
+  'From which registration date should new users displayed as new?' => 'От коя дата на регистрация новите потребители трябва да се показват като нови?',
+  'Save' => 'Запазване',
+  'The number of most active users that will be shown.' => 'Броят на най-активните потребители, които ще бъдат показани.',
+  'The panel title for the dashboard widget.' => 'Заглавието на панела за приспособлението на таблото за управление.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Тази стойност може да е необходима след импортиране от съществуващи потребители. Оставете я празна, ако вашата потребителска база расте естествено.',
+);
