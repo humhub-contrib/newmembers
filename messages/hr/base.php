5e7f14d1468619eea8ccb7fec06a89b20243bd6a
HumHub Translations <info@humhub.com>
2020-11-29 20:46:26 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hr/base.php
+++ messages/hr/base.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  '<strong>New Members</strong> Module Configuration' => '',
+  '<strong>New Members</strong> Module Configuration' => '<strong>Novi korisnici</strong> Modul Konfiguracija',
   'Back to modules' => 'Povratak na module',
-  'From which registration date should new users displayed as new?' => '',
+  'From which registration date should new users displayed as new?' => 'Od kojeg bi se datuma registracije novi korisnici trebali prikazivati kao novi?',
   'Save' => 'Spremi',
   'The number of most active users that will be shown.' => 'Broj najaktivnijih korisnika koji će biti prikazani.',
-  'The panel title for the dashboard widget.' => '',
-  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
+  'The panel title for the dashboard widget.' => 'Naslov ploče za widget nadzorne ploče.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Ova je vrijednost možda potrebna nakon uvoza od postojećih korisnika. Neka se isprazni ako vaša baza korisnika raste prirodno.',
 );
