4a0b28cb0ae0960eec2f4d53a3955b4b17cd08aa
HumHub Translations <info@humhub.com>
2021-02-15 18:24:19 +0100

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/sq/base.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    '<strong>New Members</strong> Module Configuration' => '',
+    'Back to modules' => '',
+    'From which registration date should new users displayed as new?' => '',
+    'Save' => '',
+    'The number of most active users that will be shown.' => '',
+    'The panel title for the dashboard widget.' => '',
+    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
+];
