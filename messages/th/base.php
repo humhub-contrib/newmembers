b3fe103aec1e828f01c71827ea9f820a21f3773c
HumHub Translations <info@humhub.com>
2021-06-10 20:14:13 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/th/base.php
+++ messages/th/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    'Save' => 'บันทึก',
-    '<strong>New Members</strong> Module Configuration' => '',
-    'Back to modules' => '',
-    'From which registration date should new users displayed as new?' => '',
-    'The number of most active users that will be shown.' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong>สมาชิกใหม่</strong> การกำหนดค่าโมดูล',
+  'Back to modules' => 'กลับไปที่โมดูล',
+  'From which registration date should new users displayed as new?' => 'ผู้ใช้ใหม่ควรแสดงเป็นผู้ใช้ใหม่ตั้งแต่วันที่ใด',
+  'Save' => 'บันทึก',
+  'The number of most active users that will be shown.' => 'จำนวนผู้ใช้ที่ใช้งานมากที่สุดที่จะแสดง',
+  'The panel title for the dashboard widget.' => 'ชื่อแผงสำหรับวิดเจ็ตแดชบอร์ด',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'ค่านี้อาจจำเป็นหลังจากนำเข้าจากผู้ใช้ที่มีอยู่ ปล่อยให้ว่างเปล่าหากฐานผู้ใช้ของคุณเติบโตตามธรรมชาติ',
+);
