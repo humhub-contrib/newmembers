d153d4fde48e088310cc8849869dd0c11d748c8b
HumHub Translations <info@humhub.com>
2021-06-11 10:23:53 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/el/base.php
+++ messages/el/base.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  '<strong>New Members</strong> Module Configuration' => '',
+  '<strong>New Members</strong> Module Configuration' => 'Ρυθμίσεις ενότητας <strong>Νέων Μελών</strong>',
   'Back to modules' => 'Επιστροφή στις ενότητες',
-  'From which registration date should new users displayed as new?' => '',
+  'From which registration date should new users displayed as new?' => 'Από ποιά ημ/νία εγγραφής θα εμφανίζονται οι νέοι χρήστες ως νέοι;',
   'Save' => 'Αποθήκευση',
-  'The number of most active users that will be shown.' => '',
-  'The panel title for the dashboard widget.' => '',
-  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
+  'The number of most active users that will be shown.' => 'Ο μέγιστος αριθμός ενεργών χρηστών που θα φαίνεται.',
+  'The panel title for the dashboard widget.' => 'Ο τίτλος του πάνελ για το widget του ταμπλό.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Η τιμή αυτή είναι ίσως απαραίτητη μετά από εισαγωγή υπαρχόντων χρηστών. Αφήστε την κενή εάν η βάση χρηστών μεγαλώνει οργανικά.',
 );
