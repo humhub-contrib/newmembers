85b9a5b603c9bad97c6ec3fb955526bbd2d338ed
HumHub Translations <info@humhub.com>
2021-12-07 18:54:28 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/base.php
+++ messages/nl/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => '<strong>Nieuwe leden strong&gt; moduleconfiguratie</strong>',
-    'Back to modules' => 'Terug naar modules',
-    'From which registration date should new users displayed as new?' => 'Vanaf welke registratiedatum moeten nieuwe gebruikers als nieuw worden weergegeven?',
-    'The number of most active users that will be shown.' => 'Het aantal meest actieve gebruikers dat wordt getoond.',
-    'The panel title for the dashboard widget.' => 'Titel in het dashboard',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Deze waarde is misschien nodig na een import van bestaande gebruikers. Laat het leeg als uw gebruikersbestand vanzelf groeit.',
-    'For guests' => '',
-    'For logged in members' => '',
-    'Show on dashboard' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong>Nieuwe leden strong&gt; moduleconfiguratie</strong>',
+  'Back to modules' => 'Terug naar modules',
+  'For guests' => 'Voor gasten',
+  'For logged in members' => 'Voor aangemelde leden',
+  'From which registration date should new users displayed as new?' => 'Vanaf welke registratiedatum moeten nieuwe gebruikers als nieuw worden weergegeven?',
+  'Show on dashboard' => 'Weergeven op het dashboard',
+  'The number of most active users that will be shown.' => 'Het aantal meest actieve gebruikers dat wordt getoond.',
+  'The panel title for the dashboard widget.' => 'Titel in het dashboard',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Deze waarde is misschien nodig na een import van bestaande gebruikers. Laat het leeg als uw gebruikersbestand vanzelf groeit.',
+);
--- messages/sv/base.php
+++ messages/sv/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => '<strong> Nya medlemmar </strong> Modulkonfiguration',
-    'Back to modules' => 'Tillbaka till moduler',
-    'From which registration date should new users displayed as new?' => 'Från vilket registreringsdatum ska nya användare visas som nya?',
-    'The number of most active users that will be shown.' => 'Antalet aktiva användare som ska visas',
-    'For guests' => '',
-    'For logged in members' => '',
-    'Show on dashboard' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong> Nya medlemmar </strong> Modulkonfiguration',
+  'Back to modules' => 'Tillbaka till moduler',
+  'For guests' => 'För gäster',
+  'For logged in members' => 'För inloggade medlemmar',
+  'From which registration date should new users displayed as new?' => 'Från vilket registreringsdatum ska nya användare visas som nya?',
+  'Show on dashboard' => 'Visa på tidslinjen',
+  'The number of most active users that will be shown.' => 'Antalet aktiva användare som ska visas',
+  'The panel title for the dashboard widget.' => 'Rubriken för widgeten för tidslinjen.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Detta värde är möjligen nödvändigt efter en import från befintliga användare. Låt det bli tomt om din användargrupp växer naturligt.',
+);
