1a04a4662949a48c3bb1cd3debc5f0ef47b11fac
HumHub Translations <info@humhub.com>
2022-04-13 21:26:21 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => '<strong>Neue Benutzer</strong> Modul-Konfiguration',
-    'Back to modules' => 'Zurück zu den Modulen',
-    'From which registration date should new users displayed as new?' => 'Ab welchem Registrier-Datum sollen Benutzer als „neu“ angezeigt werden?',
-    'The number of most active users that will be shown.' => 'Wie viele aktivste Nutzer sollen angezeigt werden?',
-    'The panel title for the dashboard widget.' => 'Der Titel für das Dashboard-Widget.',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Dieser Wert ist möglicherweise notwendig, falls bestehende Nutzer importiert wurden. Dieses Feld leer lassen wenn die Nutzeranzahl natürlich wächst.',
-    'For guests' => '',
-    'For logged in members' => '',
-    'Show on dashboard' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong>Neue Benutzer</strong> Modul-Konfiguration',
+  'Back to modules' => 'Zurück zu den Modulen',
+  'For guests' => 'Für Gäste',
+  'For logged in members' => 'Für Eingeloggte Mitglieder',
+  'From which registration date should new users displayed as new?' => 'Ab welchem Registrier-Datum sollen Benutzer als „neu“ angezeigt werden?',
+  'Show on dashboard' => 'Auf dem Dashboard anzeigen',
+  'The number of most active users that will be shown.' => 'Wie viele aktivste Nutzer sollen angezeigt werden?',
+  'The panel title for the dashboard widget.' => 'Der Titel für das Dashboard-Widget.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Dieser Wert ist möglicherweise notwendig, falls bestehende Nutzer importiert wurden. Dieses Feld leer lassen wenn die Nutzeranzahl natürlich wächst.',
+);
--- messages/fr/base.php
+++ messages/fr/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => 'Configuration du module des <strong>Nouveaux membres</strong>',
-    'Back to modules' => 'Retour aux modules',
-    'From which registration date should new users displayed as new?' => 'Jusqu\'à quand les nouveaux utilisateurs doivent-ils être affichés comme nouveaux ?',
-    'The number of most active users that will be shown.' => 'Le nombre de membres devant être affiché.',
-    'The panel title for the dashboard widget.' => 'Le titre du panneau pour le widget de tableau de bord.',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Cette valeur est peut-être nécessaire après une importation d\'utilisateurs existants. Laissez-le vide si votre base d\'utilisateurs se développe naturellement.',
-    'For guests' => '',
-    'For logged in members' => '',
-    'Show on dashboard' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => 'Configuration du module <strong>Nouveaux membres</strong>',
+  'Back to modules' => 'Retour aux modules',
+  'For guests' => 'Pour les visiteurs',
+  'For logged in members' => 'Pour les membres connectés',
+  'From which registration date should new users displayed as new?' => 'Jusqu\'à quand les nouveaux utilisateurs doivent-ils être affichés comme nouveaux ?',
+  'Show on dashboard' => 'Afficher dans le fil d’actualités',
+  'The number of most active users that will be shown.' => 'Le nombre de membres devant être affiché.',
+  'The panel title for the dashboard widget.' => 'Le titre du panneau pour le widget de tableau de bord.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Cette valeur est peut-être nécessaire après une importation d\'utilisateurs existants. Laissez-le vide si votre base d\'utilisateurs se développe naturellement.',
+);
