15b62c7aadbeef04029cedf539f4515593fe48bf
HumHub Translations <hello@humhub.com>
2022-10-12 11:00:05 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/nb-NO/base.php
+++ messages/nb-NO/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Back to modules' => 'Tilbake til moduler',
-    'The number of most active users that will be shown.' => 'Det høyeste nummer av aktive brukere som vil vises',
-    '<strong>New Members</strong> Module Configuration' => '',
-    'For guests' => '',
-    'For logged in members' => '',
-    'From which registration date should new users displayed as new?' => '',
-    'Show on dashboard' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '',
+  'Back to modules' => 'Tilbake til moduler',
+  'For guests' => '',
+  'For logged in members' => '',
+  'From which registration date should new users displayed as new?' => '',
+  'Show on dashboard' => '',
+  'The number of most active users that will be shown.' => 'Antall mest aktive brukere som vil vises',
+  'The panel title for the dashboard widget.' => '',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
+);
