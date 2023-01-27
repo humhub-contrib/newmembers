c80022858d529c0569496e3e2274ae393e6310bb
HumHub Translations <hello@humhub.com>
2023-01-27 16:20:06 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/base.php
+++ messages/it/base.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => 'Configurazione modulo <strong>Nuovi membri</strong>',
-    'Back to modules' => 'Indietro ai moduli',
-    'For guests' => 'Per gli ospiti',
-    'For logged in members' => 'Per i membri collegati',
-    'From which registration date should new users displayed as new?' => 'Da quale data registrazione gli utenti dovrebbero essere mostrati come nuovi?',
-    'Show on dashboard' => 'Mostra sulla Dashboard',
-    'The number of most active users that will be shown.' => 'Il numero degli utenti più attivi che verrà mostrato',
-    'The panel title for the dashboard widget.' => 'Il titolo del pannello per la Widget dashboard.',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Questo valore è forse necessario dopo un\'importazione da utenti esistenti. Lascialo vuoto se la tua base di utenti cresce in modo naturale.',
-    'New Members' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => 'Configurazione modulo <strong>Nuovi membri</strong>',
+  'Back to modules' => 'Indietro ai moduli',
+  'For guests' => 'Per gli ospiti',
+  'For logged in members' => 'Per i membri collegati',
+  'From which registration date should new users displayed as new?' => 'Da quale data registrazione gli utenti dovrebbero essere mostrati come nuovi?',
+  'New Members' => 'Nuovi membri',
+  'Show on dashboard' => 'Mostra sulla Dashboard',
+  'The number of most active users that will be shown.' => 'Il numero degli utenti più attivi che verrà mostrato',
+  'The panel title for the dashboard widget.' => 'Il titolo del pannello per la Widget dashboard.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Questo valore è forse necessario dopo un\'importazione da utenti esistenti. Lascialo vuoto se la tua base di utenti cresce in modo naturale.',
+);
