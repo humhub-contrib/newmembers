1dd7a0fda2e8923365f3afe76db949fc2349ac7d
HumHub Translations <hello@humhub.com>
2023-04-20 14:52:13 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/base.php
+++ messages/ja/base.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    'Back to modules' => 'モジュールへ戻る',
-    'The number of most active users that will be shown.' => '表示される最もアクティブなユーザーの数。',
-    '<strong>New Members</strong> Module Configuration' => '',
-    'For guests' => '',
-    'For logged in members' => '',
-    'From which registration date should new users displayed as new?' => '',
-    'New Members' => '',
-    'Show on dashboard' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong>新メンバー</strong> モジュール構成',
+  'Back to modules' => 'モジュールへ戻る',
+  'For guests' => 'ゲスト用',
+  'For logged in members' => 'ログイン会員の方へ',
+  'From which registration date should new users displayed as new?' => '新規ユーザーが新規として表示されるのは、いつからの登録日ですか？',
+  'New Members' => '新メンバー',
+  'Show on dashboard' => 'ダッシュボードに表示',
+  'The number of most active users that will be shown.' => '表示される最もアクティブなユーザーの数。',
+  'The panel title for the dashboard widget.' => 'ダッシュボード ウィジェットのパネルタイトル。',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'この値は、既存のユーザーからのインポート後に必要になる場合があります。ユーザーベースが自然に成長する場合は、空にします。',
+);
