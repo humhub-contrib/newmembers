fa1659c7e5a2fa1123d8e78287aa448813595075
HumHub Translations <hello@humhub.com>
2024-02-18 18:19:53 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sk/base.php
+++ messages/sk/base.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => '',
-    'Back to modules' => '',
-    'For guests' => '',
-    'For logged in members' => '',
-    'From which registration date should new users displayed as new?' => '',
-    'New Members' => '',
-    'Show on dashboard' => '',
-    'The number of most active users that will be shown.' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '',
+  'Back to modules' => 'Späť k modulom',
+  'For guests' => '',
+  'For logged in members' => '',
+  'From which registration date should new users displayed as new?' => '',
+  'New Members' => '',
+  'Show on dashboard' => '',
+  'The number of most active users that will be shown.' => '',
+  'The panel title for the dashboard widget.' => '',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
+);
--- messages/sr/base.php
+++ messages/sr/base.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => '',
-    'Back to modules' => '',
-    'For guests' => '',
-    'For logged in members' => '',
-    'From which registration date should new users displayed as new?' => '',
-    'New Members' => '',
-    'Show on dashboard' => '',
-    'The number of most active users that will be shown.' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong>Novi korisnici</strong> konfiguracija modula',
+  'Back to modules' => 'Povratak na module',
+  'For guests' => '',
+  'For logged in members' => '',
+  'From which registration date should new users displayed as new?' => 'Od kojeg bi se datuma registracije novi korisnici trebali prikazivati kao novi?',
+  'New Members' => '',
+  'Show on dashboard' => '',
+  'The number of most active users that will be shown.' => 'Broj najaktivnijih korisnika koji će biti prikazani.',
+  'The panel title for the dashboard widget.' => 'Naslov za widget startnog panela.',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Ova je vrednost možda potrebna nakon uvoza od postojećih korisnika. Neka se isprazni ako vaša baza korisnika raste prirodno.',
+);
