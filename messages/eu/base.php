997f569731c1deaa38e77b332d2244a4b38031c9
HumHub Translations <hello@humhub.com>
2024-03-14 15:30:10 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/eu/base.php
+++ messages/eu/base.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    '<strong>New Members</strong> Module Configuration' => '',
-    'Back to modules' => '',
-    'For guests' => '',
-    'For logged in members' => '',
-    'From which registration date should new users displayed as new?' => '',
-    'New Members' => '',
-    'Show on dashboard' => '',
-    'The number of most active users that will be shown.' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '<strong>Kide Berriak</strong> Moduluaren Konfigurazioa',
+  'Back to modules' => 'Itzuli moduluetara',
+  'For guests' => 'Gonbidatuentzat',
+  'For logged in members' => 'Saioa hasitako kideentzat',
+  'From which registration date should new users displayed as new?' => 'Zein erregistro-datatik aurrera agertu behar dira erabiltzaile berriak berri gisa?',
+  'New Members' => 'Kide Berriak',
+  'Show on dashboard' => 'Erakutsi oholan',
+  'The number of most active users that will be shown.' => 'Erakutsiko den erabiltzaile aktiboenen kopurua.',
+  'The panel title for the dashboard widget.' => 'Ohola widget-aren panel izenburua',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => 'Balio hau agian beharrezkoa izango da lehendik dagoen erabiltzaileren batengandik inportatu ondoren. Hutsik utzi zure erabiltzaileen datu-basea era naturalean hazten ari bada.',
+);
