17f84976785c904f2b8f56c2a79f305c295f1565
HumHub Translations <hello@humhub.com>
2024-06-18 16:01:36 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ru/base.php
+++ messages/ru/base.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    'Back to modules' => 'Назад к модулям',
-    'The number of most active users that will be shown.' => 'Количество активных пользователей, которое будет отображаться.',
-    '<strong>New Members</strong> Module Configuration' => '',
-    'For guests' => '',
-    'For logged in members' => '',
-    'From which registration date should new users displayed as new?' => '',
-    'New Members' => '',
-    'Show on dashboard' => '',
-    'The panel title for the dashboard widget.' => '',
-    'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
-];
+return array (
+  '<strong>New Members</strong> Module Configuration' => '',
+  'Back to modules' => 'Назад к модулям',
+  'For guests' => 'Для гостей',
+  'For logged in members' => '',
+  'From which registration date should new users displayed as new?' => '',
+  'New Members' => 'Новый пользователь',
+  'Show on dashboard' => '',
+  'The number of most active users that will be shown.' => 'Количество активных пользователей, которое будет отображаться.',
+  'The panel title for the dashboard widget.' => '',
+  'This value is maybe necessary after an import from existing users. Let it empty if your user base grows naturally.' => '',
+);
